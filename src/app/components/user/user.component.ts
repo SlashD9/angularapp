5dedd37d4c9a9f741231672def7d0e1571a65417
C Daly <colin.p.s.daly@gmail.com>
2020-07-04 22:05:42 +0000

    Added Properties and methods to user.component

--- src/app/components/user/user.component.ts
+++ src/app/components/user/user.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { elementEventFullName } from '@angular/compiler/src/view_compiler/view_compiler';
+import { Message } from '@angular/compiler/src/i18n/i18n_ast';
 
 @Component({
     selector: 'app-user',
@@ -12,12 +13,28 @@ export class UserComponent {
     firstName = 'John';
     lastName = 'Doe';
     age = 30;
+    address = {
+        street: '50 Main St',
+        city: 'Boston',
+        state: 'MA'
+    };
     fullName;
+    message;
 
     // Method
     constructor() {
-        this.fullName = this.firstName + ' ' + this.lastName;
-        return this.fullName;    
+        this.sayHello();  
+        console.log(this.age);
+        this.hasBirthday();
+        console.log(this.age);
+    }
+    // Custom Methods
+    sayHello() {
+        this.message = 'Hello ' + this.firstName + ' ' + this.lastName;
+    }
+
+    hasBirthday() {
+        this.age += 1;
     }
     
  }
