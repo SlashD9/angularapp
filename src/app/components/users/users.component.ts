6b98a9435dd8d1af27a63e0ef52b4d637465ba2b
C Daly <colin.p.s.daly@gmail.com>
2020-07-11 10:40:10 +0100

    vid18-ngif using a timeout to mimic loading users

--- src/app/components/users/users.component.ts
+++ src/app/components/users/users.component.ts
@@ -9,47 +9,48 @@ import { User } from '../../models/User';
 export class UsersComponent implements OnInit {
   users: User[];
   showExtended: Boolean = true;
+  loaded: Boolean = false;
 
   constructor() { }
 
   ngOnInit() {
-    this.users = [
-      {
-        firstName: 'John',
-        lastName: 'Doe',
-        age: 30,
-        address: {
-            street: '50 main street',
-            city: 'Dun Laoghaire',
-            state: 'Dublin'
-        }
-      },
-      {
-        firstName: 'Phil',
-        lastName: 'Johnson',
-        age: 33,
-        address: {
-            street: '55 small street',
-            city: 'Bray',
-            state: 'Wicklow'
-        }
-      },
-      {
-        firstName: 'Jenny',
-        lastName: 'Hill',
-        age: 25,
-        address: {
-            street: '45 Slow Street',
-            city: 'Arklow',
-            state: 'Wicklow'
-        }
-      }
-    ];
+    setTimeout(() => {
+      this.users = [
+            {
+              firstName: 'John',
+              lastName: 'Doe',
+              age: 30,
+              address: {
+                  street: '50 main street',
+                  city: 'Dun Laoghaire',
+                  state: 'Dublin'
+              }
+            },
+            {
+              firstName: 'Phil',
+              lastName: 'Johnson',
+              age: 33,
+              address: {
+                  street: '55 small street',
+                  city: 'Bray',
+                  state: 'Wicklow'
+              }
+            },
+            {
+              firstName: 'Jenny',
+              lastName: 'Hill',
+              age: 25,
+              address: {
+                  street: '45 Slow Street',
+                  city: 'Arklow',
+                  state: 'Wicklow'
+              }
+            }
+          ];
 
-      this.addUser({
-        firstName: 'Frank',
-        lastName: 'James',
-    })
+        this.loaded = true;
+    }, 2000);
+    
   }
   addUser(user: User) {
     this.users.push(user);
